2920f7b4be897fb3d459eebab48e6aa69204b394
BrandonNivin <brandonnivin@yahoo.com>
2025-06-12 13:11:29 -0400

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 12
+if (amount < 12){
+    console.log('small number');
+}
+else if(amount > 12){
+    console.log('large number');
+}
+else{
+    console.log('correct number!')
+}
---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules 
+// module     - info about current module (file)
+// process    - info about env WHERE THE PROGRAM IS BEING EXECUTED
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('hello world')
+}, 1000)
