85d8cede74d8b2385b8569d09db6e3aa17d32252
BrandonNivin <brandonnivin@yahoo.com>
2025-06-12 15:18:45 -0400

    updated app.js

--- app.js
+++ app.js
@@ -13,6 +13,12 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
+// To update changed files to repository...
+// git status - checks which files are modified
+// git add <filename> - stages files to be updated in repository
+// git commit -m "Update message(can be whatever)" - ready to push to repository
+// git push - pushes the changes to repository
+
 const _= require('lodash');
 
 const items = [1,[2, [3, [4]]]]
