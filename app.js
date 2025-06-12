abc2e58d852ea3dcdf949141cf5bc92c2eda2c27
BrandonNivin <brandonnivin@yahoo.com>
2025-06-12 15:14:13 -0400

    updated app.js to current

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ const _= require('lodash');
 const items = [1,[2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems);
-console.log('We finally have an operating server! poop ')
+console.log('We finally have an operating server!')
 
